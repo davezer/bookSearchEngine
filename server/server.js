93fb07a609c895c00fe2e13b925abae37f9eaec1
davezer <davezer27@gmail.com>
2022-02-25 16:16:02 -0500

    organized files and updated server.js to use apollo

--- server/server.js
+++ server/server.js
@@ -3,8 +3,22 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// setting apollo server to use typeDefs, resolvers and auth for context
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrating apollo server with express app as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +32,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
