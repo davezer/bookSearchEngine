77c17ac8321ca8b6f91cec3dc636a0782cb04576
davezer <davezer27@gmail.com>
2022-02-25 20:30:34 -0500

    heroku roulette

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(`mongodb+srv://daveUconn:Doozer1127@cluster0.yikty.mongodb.net/bookSearchEngine?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const dotenv = require('dotenv');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
