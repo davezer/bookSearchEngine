5829d018bd0f5c35ba11284ba6324fd94363fcbf
davezer <davezer27@gmail.com>
2022-02-25 20:31:33 -0500

    im having tons of fun with heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://daveUconn:Doozer1127@cluster0.yikty.mongodb.net/bookSearchEngine?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://daveUconn:Doozer1127@cluster0.yikty.mongodb.net/bookSearchEngine`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
